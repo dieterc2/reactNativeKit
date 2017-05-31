e93da5a7ae309f0311618396c8045cd89adae46b
Chris Dieter <c.dieter@lilly.com>
2017-05-30 22:45:27 -0400

    adding navigation

--- ReactKickStart/js/index.js
+++ ReactKickStart/js/index.js
@@ -1,49 +1,20 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text, StyleSheet, View } from 'react-native';
-import { Container, Header, Left, Icon, Body, Title, Button, Right, Content, Footer, FooterTab } from 'native-base';
-import Home from './home'
-import weather from './controllers/weather'
+import { Navigation } from 'react-native-navigation';
 
-export default class Index extends Component {
-  render() {  
-    return (
-      <Container>
-        <Header>
-            <Left>
-                <Button transparent>
-                    <Icon name='menu' />
-                </Button>
-            </Left>
-            <Body>
-                <Title>ReactKit</Title>
-            </Body>
-            <Right />
-        </Header>
-        <Content>
-          <Home title="Jimmy" />
-          <Text>{this.props.currentTemp}</Text>
-        </Content>
-        <Footer>
-            <FooterTab>
-                <Button full>
-                    <Text>Footer</Text>
-                </Button>
-            </FooterTab>
-        </Footer>
-    </Container>
-    );
-  }
-}
+import { registerScreens } from './screens/ScreenIndex';
 
-const styles = StyleSheet.create({
-  header: {
-    color: 'blue'
-  },
-  headerBox: {
-    flex: 1,
-    height: 50,
-    width: 50,
-    backgroundColor: 'powderblue',
-    alignItems: 'center'
-  }
+registerScreens();
+
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'One',
+      screen: 'example.FirstTabScreen', // this is a registered name for a screen
+      title: 'Screen One'
+    },
+    {
+      label: 'Two',
+      screen: 'example.SecondTabScreen',
+      title: 'Screen Two'
+    }
+  ]
 });
